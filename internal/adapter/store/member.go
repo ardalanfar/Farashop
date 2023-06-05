97e45388a663868720b39353846db1fd6c341b56
[Arash Ardalan] <[ar4sh_ardalan@yahoo.com]>
2023-06-05 12:08:46 +0330

    edit sotr member

--- internal/adapter/store/member.go
+++ internal/adapter/store/member.go
@@ -6,7 +6,9 @@ import (
 )
 
 func (conn DbConn) ShowOrders(ctx context.Context, userID uint) ([]dto.Showorders, error) {
-	var Showorders []dto.Showorders
+	var (
+		Showorders []dto.Showorders
+	)
 
 	//get all information
 	cheek := conn.Db.WithContext(ctx).Table("orders").Select("orders.id", "orders.number", "orders.buy_time", "orders.status", "products.name").Joins("JOIN products ON products.id = orders.product_id").Where("user_id", userID).Find(&Showorders)
